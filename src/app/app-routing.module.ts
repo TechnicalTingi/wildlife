45db0436e69d97477ed581a3cad6365ca8221358
TechnicalTingi <teachnicaltingi@gmail.com>
2021-07-08 08:34:20 +0530

    adding routing error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorpageComponent } from './features/404/errorpage/errorpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'**', component:ErrorpageComponent},
+ { path: '',
+    redirectTo: '',
+    pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule} from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './shared/primeng/primeng.module';
+import { ErrorpageComponent } from './features/404/errorpage/errorpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorpageComponent
   ],
   imports: [
     BrowserModule,
